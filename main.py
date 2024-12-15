6e38b5fde667ba8a115e2871c59c0c1d0db25784
raffkelly <raffkelly@gmail.com>
2024-12-15 00:02:35 -0500

    score is tracked and reported in console after game over

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from shot import Shot
 
 def main():
     pygame.init()
+    pygame.font.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    score = 0
+    font = pygame.font.SysFont("Consolas", 30)
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
@@ -23,29 +26,35 @@ def main():
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
+    
 
     background = pygame.image.load("space.jpg").convert()
 
 
     while True:
-        
+        score_surface = font.render(f"Score: {score}", False, "white")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.blit(background, (0, 0))
+        screen.blit(score_surface, (0, 0))
         for thing in updatable:
             thing.update(dt)
         for asteroid in asteroids:
             if asteroid.collision_check(player):
                 print("Game over!")
+                print(f"Final Score: {score}")
                 return
             for shot in shots:
                 if asteroid.collision_check(shot):
                     asteroid.split()
                     shot.kill()
+                    score += 1
         for thing in drawable:
             thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
+        
 if __name__ == "__main__":
     main()
